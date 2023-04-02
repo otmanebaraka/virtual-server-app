f6c26cbaa8746f0cb5366933c599d69de5cedf7e
Otmane <samdev.br@gmail.com>
2023-04-02 04:54:27 +0000

    fix get servers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VirtualServer } from './interfaces/virtual-server';
+import { BackendService } from './services/backend.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private backendService: BackendService) { }
+
+  ngOnInit(): void {
+    this.backendService.getServers().subscribe(
+      (servers: VirtualServer[]) => {
+        console.log(servers);
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
 }
